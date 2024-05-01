8e044742aae5339387eb586864ee0f8da1399dd3
G-TECH <haya98166@gmail.com>
2024-05-02 00:23:31 +0300

    shared prefrenced onBoarding # Conflicts: #	lib/Presentation/Screens/Authentication/forget_screen.dart #	lib/Presentation/Screens/Authentication/login_screen.dart #	lib/Presentation/Screens/Authentication/reset_password_screen.dart #	lib/Presentation/Screens/Authentication/sign_up_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 // import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
- import 'package:go_router/go_router.dart';
 import 'package:grady/Presentation/config/routes.dart';
 import 'package:grady/firebase_options.dart';
 
